3e44a11aa6ff0fd72c2276ef44124caaeaa60146
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-12-11 11:25:31 +0300

    feat: the time limit exceed for k closest points is solved

--- Lab_practices/K_closest_points_to_Origin.py
+++ Lab_practices/K_closest_points_to_Origin.py
@@ -1,24 +1,39 @@
-
+import math
+import numpy
 class Solution(object):
     def kClosest(self, points, k):
         
         # closest_distance = Solution.calcDistance(points[0])
-        temp = []
-        for i in range(0, len(points)):
-            min = i
-            for j in range(i+1, len(points)):
-                if(((points[min][0])**2 + (points[min][1])**2)>((points[j][0])**2 + (points[j][1])**2)):
-                    min = j
-            points[min], points[i] = points[i], points[min]
-        for i in range(k):
-            temp.append(points[i])
-                
-        return temp
-        """
-        :type points: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
+        # points.sort(key )
+        # for i in range(0, len(points)):
+        #     min = i
+        #     for j in range(i+1, len(points)):
+        #         if(((points[min][0])**2 + (points[min][1])**2)>((points[j][0])**2 + (points[j][1])**2)):
+        #             min = j
+        #     points[min], points[i] = points[i], points[min]
+        # return points[:k]
+
+        
+        # Second method of implementation
+
+        # temp = [0] * len(points)
+
+        for i in range(len(points)):
+                distance = self.calc_distance(points[i])
+                points[i] = [distance, points[i]]
+
+        points.sort(key = lambda point: point[0])
+
+        return [points[i][1] for i in range(k)]
+        
+    def calc_distance(self, point):
+        return math.sqrt((point[1])**2 + (point[0])**2)
+
+        
+
+
+
+
+
 soln = Solution()
-print(soln.kClosest([[1,3],[-2,2]],
-1138))
+print(soln.kClosest([[2,2],[2,-4],[3,3]], 2))
