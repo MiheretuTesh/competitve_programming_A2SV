53a4df010bd81f7ead10815354dafee271f7a43b
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-12-15 22:34:38 +0300

    feat: array_not_equal to neighbors

--- Lab_practices/Array With Elements Not Equal to Average of Neighbors.py
+++ Lab_practices/Array With Elements Not Equal to Average of Neighbors.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        s = len(nums)
-        count = 1
-        if(s < 3):
-            return nums
-        else:
-            while(count < s-1):
-                if((nums[count-1]+nums[count+1])/2 == nums[count]):
 
-                    temp = nums.pop(count)
-                    nums.append(temp)
+        s = len(nums)
+        lst = []
+        count = 1
+        if(s<3):
+            return nums
+        else:    
+            while(count < s-1):
+                if((nums[count-1] + nums[count+1]) / 2 == nums[count]):
+                    nums.append(nums.pop(count))
+                    count = 1
                 else:
                     count += 1
             return nums
