a9961ce85fd9436eefb9b91a9b8fdd9b96753ce4
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-12-21 18:42:57 +0300

    feat: Daily Temprature

---
+++ Lab_practices/Daily Temperatures.py
@@ -0,0 +1,19 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        size = len(temperatures)
+        stack = []
+        result = [0] * size 
+        for i in range(size):
+            if not stack:
+                stack.append((temperatures[i], i))
+            else:
+                while stack and stack[-1][0] < temperatures[i]:
+                    value, index = stack.pop()
+                    result[index] = i - index
+                stack.append((temperatures[i], i))
+        return result
+                    
+                    
+                    
+                
+                
