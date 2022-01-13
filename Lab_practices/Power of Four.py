b9c2f03cbd407d7bf25ce4553d486b1d7f24dac4
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2022-01-13 23:44:57 +0300

    feat: power of Four

---
+++ Lab_practices/Power of Four.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n < 1:
+            return False
+        if n == 1:
+            return True
+        if n % 4 == 0:
+            return self.isPowerOfFour(n//4)
+        return False
+        # if n < 1:
+        #     return False
+        # while n%4 == 0:
+        #     n /= 4
+        # return n==1
+    
+    
+        
